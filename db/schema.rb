1829b4d78ec3e10fcac4b7f97440ac4303262922
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-11 14:52:25 -0400

    tasks now displaying

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_174917) do
+ActiveRecord::Schema.define(version: 2020_08_11_175021) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
