717771d1784375a31be6c317bed4fe76a4234090
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-11 16:05:21 -0400

    back now working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
