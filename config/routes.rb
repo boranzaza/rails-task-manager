f23ac03347db11c20cd55700d9a3179db19e1d45
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-11 17:35:29 -0400

    remove method now working

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch 'tasks/:id', to: 'tasks#update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
