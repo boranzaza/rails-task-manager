321a1bd345272e4c080b9c94334fee7dd35fb623
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-11 17:24:31 -0400

    updating now works

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
 
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks', to: 'tasks#update'
+  patch 'tasks/:id', to: 'tasks#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
