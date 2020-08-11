c23a9bf5dcfb9b6fabc4bdc20020c9159a44b0ed
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-11 16:55:51 -0400

    Update integrated but not working well

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
   post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks', to: 'tasks#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
